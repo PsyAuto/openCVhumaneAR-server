cd7a7915c101ed9df71fb9aaa22534c71dc7ff51
Marios Iakovidis <marios.iakovidis@gmail.com>
2023-06-21 20:18:16 +0300

    api collection

--- routes/UserDAO.js
+++ routes/UserDAO.js
@@ -5,43 +5,45 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   userID: {
     type: String,
-    required: true,
+    required: false,
     unique: true,
     description: "Unique identifier for the user"
-  },
-  SelectedMarkerIndex: {
-    type: Number,
-    required: true,
-    description: "Index of the selected marker"
-  },
-  NeighborKeywords: {
-    type: [String],
-    required: true,
-    description: "Array of keywords associated with the user's neighbors"
-  },
-  MyKeywords: {
-    type: [String],
-    required: true,
-    description: "Array of keywords associated with the user"
-  },
-  NewKeywords: {
-    type: [String],
-    description: "Array of new keywords added by the user"
-  },
-  CurrentStage: {
-    type: Number,
-    required: true,
-    description: "Current stage of the user's progress"
-  },
-  MyArticle: {
-    type: String,
-    description: "Article associated with the user"
-  },
-  NeighborArticles: {
-    type: [String],
-    description: "Array of articles associated with the user's neighbors"
+    },
+    SelectedMarkerIndex: {
+      type: Number,
+      required: false,
+      description: "Index of the selected marker"
+    },
+    NeighborKeywords: {
+      type: [String],
+      required: false,
+      description: "Array of keywords associated with the user's neighbors"
+    },
+    MyKeywords: {
+      type: [String],
+      required: false,
+      description: "Array of keywords associated with the user"
+    },
+    NewKeywords: {
+      type: [String],
+      description: "Array of new keywords added by the user"
+    },
+    CurrentStage: {
+      type: Number,
+      required: false,
+      default: 1,
+      description: "Current stage of the user's progress"
+    },
+    MyArticle: {
+      type: String,
+      description: "Article associated with the user"
+    },
+    NeighborArticles: {
+      type: [String],
+      description: "Array of articles associated with the user's neighbors"
+    }
   }
-});
+);
 
 // Add indexes for frequently queried fields
 UserSchema.index({ userID: 1 });
@@ -104,6 +106,73 @@ const UserDAO = {
    */
   delete: function(id) {
     return User.findByIdAndDelete(id);
+  },
+
+  /**
+   * Finds all users in the database using userID.
+   * @returns {Promise} A Promise that resolves to an array of users.
+   */
+  findByUserID: function(userID) {
+    return User.find({ userID: userID });
+  },
+
+  /**
+   * Finds a user by userID in the database.
+   * @param {string} userID - The userID of the user to find.
+   * @returns {Promise} A Promise that resolves to the found user.
+   */
+  findByUserID: function(userID) {
+    return User.findOne({ userID: userID });
+  },
+
+  /**
+   * Updates a user in the database.
+   * @param {string} userID - The userID of the user to update.
+   * @param {Object} user - The updated user data.
+   * @returns {Promise} A Promise that resolves to the updated user.
+   */
+  updateByUserID: function(userID, user) {
+    return User.findOneAndUpdate({ userID: userID }, user);
+  },
+
+  /**
+   * Deletes a user from the database.
+   * @param {string} userID - The userID of the user to delete.
+   * @returns {Promise} A Promise that resolves when the user is deleted.
+   */
+  deleteByUserID: function(userID) {
+    return User.findOneAndDelete({ userID: userID });
+  },
+
+  /**
+   * Sets the selected marker index for a user in the database.
+   * @param {string} userID - The ID of the user to update.
+   * @param {number} selectedMarkerIndex - The new selected marker index for the user.
+   * @returns {Promise} A Promise that resolves to the updated user.
+   */
+  setSelectedMarkerIndex: async function(userID, selectedMarkerIndex) {
+    // Find the user with the given userID
+    const user = await User.findOne({ userID: userID });
+
+    // If the user doesn't exist, throw an error
+    if (!user) {
+      throw new Error(`User with ID ${userID} not found`);
+    }
+
+    // Check if the selected marker index is already in use by another user
+    const existingUser = await User.findOne({ SelectedMarkerIndex: selectedMarkerIndex });
+
+    // If the selected marker index is already in use, throw an error
+    if (existingUser) {
+      throw new Error(`Selected marker index ${selectedMarkerIndex} is already in use by user with ID ${existingUser.userID}`);
+    }
+
+    // Update the user's selected marker index and save to the database
+    user.SelectedMarkerIndex = selectedMarkerIndex;
+    await user.save();
+
+    // Return the updated user object
+    return user;
   }
 };
 
--- routes/users.js
+++ routes/users.js
@@ -16,6 +16,19 @@ router.get('/', (req, res) => {
     .catch(error => res.status(400).json({ error: error.message }));
 });
 
+// Get all users by userID
+router.get('/userID', (req, res) => {
+  UserDAO.find()
+    .then(users => {
+      var userIDs = [];
+      for (var i = 0; i < users.length; i++) {
+        userIDs.push(users[i].userID);
+      }
+      return res.json(userIDs);
+    })
+    .catch(error => res.status(400).json({ error: error.message }));
+});
+
 // Get a user by id
 router.get('/:id', (req, res) => {
   UserDAO.findById(req.params.id)
@@ -140,6 +153,63 @@ router.put('/:id/neighborarticles', (req, res) => {
   UserDAO.update(req.params.id, { NeighborArticles: req.body.NeighborArticles })
     .then(user => res.json(user))
     .catch(error => res.status(400).json({ error: error.message }));
+});
+
+// Get a user by userID
+router.get('/userID/:userID', (req, res) => {
+  UserDAO.findByUserID(req.params.userID)
+    .then(user => {
+      if (!user) return res.status(404).end();
+      return res.json(user);
+    })
+    .catch(error => res.status(400).json({ error: error.message }));
+});
+
+// Create a new user by userID
+router.post('/userID/:userID', async (req, res) => {
+  try {
+    const userID = req.params.userID;
+    const existingUser = await UserDAO.findByUserID(userID);
+    if (existingUser) {
+      throw new Error('User already exists');
+    }
+    const newUser = await UserDAO.create({ ...req.body, userID });
+    res.status(201).json(newUser);
+  } catch (error) {
+    if (error.message === 'User already exists') {
+      res.status(409).json({ error: error.message });
+    } else {
+      res.status(400).json({ error: error.message });
+    }
+  }
+});
+
+// Update a user by userID
+router.put('/userID/:userID', (req, res) => {
+  UserDAO.updateByUserID(req.params.userID, req.body)
+    .then(user => res.json(user))
+    .catch(error => res.status(400).json({ error: error.message }));
+});
+
+// Delete a user by userID
+router.delete('/userID/:userID', (req, res) => {
+  UserDAO.deleteByUserID(req.params.userID)
+    .then(() => res.status(204).end())
+    .catch(error => res.status(400).json({ error: error.message }));
+});
+
+// Set a user's selected marker index by userID
+router.put('/userID/:userID/selectedmarkerindex', async (req, res, next) => {
+  try {
+    const userID = req.params.userID;
+    const selectedMarkerIndex = req.body.selectedMarkerIndex;
+
+    const user = await UserDAO.setSelectedMarkerIndex(userID, selectedMarkerIndex);
+
+    res.json(user);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
